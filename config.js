316a940e7335d0b8fc645faf76eed2fc7f903a06
Siva Pallem <siva.pallem@rabobank.com>
2025-07-26 12:48:26 +0530

    changed DB to cloud
    changed DB to cloud

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Replace <username>, <password>, and <dbname> with your MongoDB credentials and database name
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/chatapp"
+  mongoURI: "mongodb+srv://root:root@cluster0.oxv3sgf.mongodb.net/chatapp"
 };
--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://personal-chat-appl.netlify.app",
     methods: ["GET", "POST"],
   },
 });
@@ -64,7 +64,7 @@ io.on("connection", (socket) => {
     console.log("User Disconnected", socket.id);
   });
 });
-
-server.listen(3001, () => {
-  console.log("SERVER RUNNING");
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`SERVER RUNNING ON PORT ${PORT}`);
 });
